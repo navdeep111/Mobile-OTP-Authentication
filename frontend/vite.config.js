ebe882c9f252a5abbaa0a34d586c8b7cf35ba623
navdeep111 <guptanavdeep9461@gmail.com>
2025-02-28 13:50:15 +0530

    vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://mobile-otp-authentication.onrender.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, "/api"),
       },
